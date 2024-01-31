42a22f11ae0cdb211a5deb03312f6dc251ead8d3
OnsKoukiPro <ons.koukipro@yahoo.com>
2024-01-31 20:31:48 +0100

    fixed bugs

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -47,7 +47,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         get_user_model().objects.create(**payload)
-        res = self.client.post(CREATE_USER_URL)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
